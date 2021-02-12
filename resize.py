636dfc401b0cd1abedbcf742f4d84c328fb9ff69
Baylenay <baylenay@gmail.com>
2021-02-12 01:28:14 +0000

    added fn for downsizing image

---
+++ resize.py
@@ -0,0 +1,13 @@
+import cv2
+
+
+def resize(image, scale=50):
+    scale_percent = scale  # percent of original size
+    width = int(image.shape[1] * scale_percent / 100)
+    height = int(image.shape[0] * scale_percent / 100)
+    dim = (width, height)
+
+    # resize image
+    resized = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
+
+    return resized
