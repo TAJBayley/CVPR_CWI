d6c4d285e107b1267b65e60d884e3d86b4d4c39f
Baylenay <baylenay@gmail.com>
2021-02-14 01:03:52 +0000

    Adjusted for real data

--- auto_correspondences.py
+++ auto_correspondences.py
@@ -11,7 +11,7 @@ from matplotlib import pyplot as plt
 
 
 class auto():
-    #Automatic keypoint detection
+    # Automatic keypoint detection
     def __init__(self, img1, img2):
         self.img1 = img1
         self.img2 = img2
@@ -20,14 +20,14 @@ class auto():
         print("Finding auto keypoints")
 
         MIN_MATCH_COUNT = 10
-        INT_TRIGGER = 1 #if this is 1, keypoints found are rounded to integer precision, to make them comparible to manual keypoints
+        INT_TRIGGER = 1  # if this is 1, keypoints found are rounded to integer precision, to make them comparible to manual keypoints
 
         img1 = self.img1
         img2 = self.img2
         print("Loaded images")
 
         # Initiate SIFT detector
-        #sift = cv2.SIFT()
+        # sift = cv2.SIFT()
         sift = cv2.SIFT_create()
         print("Initiated SIFT")
 
@@ -62,11 +62,11 @@ class auto():
             matchesMask = mask.ravel().tolist()
 
             print("matches mask:", mask)
-            #h, w, _ = img1.shape
-            #pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)
-            #dst = cv2.perspectiveTransform(pts, M)
+            # h, w, _ = img1.shape
+            # pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)
+            # dst = cv2.perspectiveTransform(pts, M)
 
-            #img2 = cv2.polylines(img2, [np.int32(dst)], True, 255, 3, cv2.LINE_AA)
+            # img2 = cv2.polylines(img2, [np.int32(dst)], True, 255, 3, cv2.LINE_AA)
 
         else:
             print
@@ -80,10 +80,10 @@ class auto():
 
         img3 = cv2.drawMatches(img1, kp1, img2, kp2, good, None, **draw_params)
         print("Saving autocorrespondence image")
-        cv2.imwrite("output_images/auto_correspondences.png",img3)
+        cv2.imwrite("output_images/auto_correspondences.png", img3)
 
-        new_src_pts = np.delete(src_pts, np.where(mask == 0), axis = 0) #remove outliers and return keypoints
-        new_dst_pts = np.delete(dst_pts, np.where(mask == 0), axis = 0)
+        new_src_pts = np.delete(src_pts, np.where(mask == 0), axis=0)  # remove outliers and return keypoints
+        new_dst_pts = np.delete(dst_pts, np.where(mask == 0), axis=0)
 
         return new_src_pts, new_dst_pts, M
 
